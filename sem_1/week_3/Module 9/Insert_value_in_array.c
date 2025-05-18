028e04863119f8e487b7b539d97d27a0bd6c4133
Arefin Ahmed <arefinahmed131@gmail.com>
2025-05-19 01:54:44 +0600

    Module 9

--- sem_1/week_3/Module 9/Insert_value_in_array.c
+++ sem_1/week_3/Module 9/Insert_value_in_array.c
@@ -3,21 +3,21 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    int ar[n + 1];
+    int ar[n + 1]; // int ar[n]; eita likhlam na keno
 
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &ar[i]);
     }
- 
+
     int ind, val;
     scanf("%d %d", &ind, &val);
- 
+
     for (int i = n; i >= ind + 1; i--)
     {
         ar[i] = ar[i - 1];
     }
-    ar[ind]=val;
+    ar[ind] = val;
 
     for (int i = 0; i <= n; i++)
     {
