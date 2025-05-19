8fab67c4d9f31556e98e83a9a822d19c01881420
Arefin Ahmed <arefinahmed131@gmail.com>
2025-05-20 01:26:26 +0600

    Module 9

--- sem_1/week_3/Module 9/Removing_value_in_array.c
+++ sem_1/week_3/Module 9/Removing_value_in_array.c
@@ -2,25 +2,25 @@
 int main()
 {
     int n;
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    int ar [n];
-    for (int i=0; i<n;i++)
+    int ar[n];
+    for (int i = 0; i < n; i++)
     {
-        scanf("%d",&ar[i]);
+        scanf("%d", &ar[i]);
     }
 
     int ind;
     scanf("%d", &ind);
 
-    for (int i = ind; i <n; i++)
+    for (int i = ind; i < n; i++)
     {
-        ar[i] = ar[i +1];
+        ar[i] = ar[i + 1];
     }
 
-    for (int i = 0; i < n-1; i++)
+    for (int i = 0; i < n - 1; i++)
     {
         printf("%d ", ar[i]);
     }
-    return 0 ;
+    return 0;
 }
---
+++ sem_1/week_3/Module 9/Swapping_two_values_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b); // user input niye ei code solve kibhave korbo?
+
+    int tmp = a;
+
+    a = b;
+    b = tmp;
+
+    printf("a = %d\nb = %d", a, b);
+    return 0;
+}
