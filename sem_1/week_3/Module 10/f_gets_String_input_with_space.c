7eb3926009e2e4202b5e05ad749f41ba38b8b385
Arefin Ahmed <arefinahmed131@gmail.com>
2025-06-21 07:19:54 +0600

    Module 10 - f_gets

--- sem_1/week_3/Module 10/f_gets_String_input_with_space.c
+++ sem_1/week_3/Module 10/f_gets_String_input_with_space.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#include <string.h>
 int main()          // When output not come add header file  #include <string.h> 
 {
     char s[50];
-    fgets(s, 13, stdin);
+    fgets(s, 13, stdin);      // fgets(s, size, stdin);
     printf("%s", s);
 
     return 0;
