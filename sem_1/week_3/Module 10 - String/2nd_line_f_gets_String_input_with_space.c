58c4e8feaa8b7719ae463395267eca8e6f248b33
Arefin Ahmed <arefinahmed131@gmail.com>
2025-06-21 11:03:52 +0600

    Module 10 - String

--- sem_1/week_3/Module 10 - String/2nd_line_f_gets_String_input_with_space.c
+++ sem_1/week_3/Module 10 - String/2nd_line_f_gets_String_input_with_space.c
@@ -3,8 +3,9 @@
 int main() // When output not come add header file  #include <string.h>
 {
     char s[23];
-    fgets(s, 13, stdin); // fgets(s, size, stdin);
-    printf("%s", s);
+    fgets(s, 23, stdin); // f_gets(s, size, stdin);
+
+    printf("%s", s);    // f_gets new line input ney
 
     return 0;
 }
