02285b0bc2d5b9dbb54e4efbde81e2ae1c828182
Arefin Ahmed <arefinahmed131@gmail.com>
2025-08-18 23:41:14 +0600

    Module 10 - String

--- sem_1/week_3/Module 10 - String/strlen_String_count_2.c
+++ sem_1/week_3/Module 10 - String/strlen_String_count_2.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-
+                    
 int main()
 {
     char s[100];
-    scanf("%s", s); // If use &s
+    scanf("%s", s); 
+    printf("%s\n", s);
+    printf("Length: %d", strlen(s));
+}
+                    // If use &s
                     /*The code might still compile, but it can cause
                     warnings, confusing behavior, or even a crash,
                     especially with newer compilers or on different
                     systems.*/
-    printf("%s\n", s);
-    printf("Length: %d", strlen(s));
-}
