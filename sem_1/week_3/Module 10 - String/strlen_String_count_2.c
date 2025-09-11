f1c25c5aac0143927bc029f8bfc8edec662a7bfe
Arefin Ahmed <arefinahmed131@gmail.com>
2025-09-11 16:50:32 +0600

    strlen use in String count

--- sem_1/week_3/Module 10 - String/strlen_String_count_2.c
+++ sem_1/week_3/Module 10 - String/strlen_String_count_2.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 #include <string.h>
-                    
+
 int main()
 {
     char s[100];
-    scanf("%s", s); 
+    scanf("%s", s);
     printf("%s\n", s);
     printf("Length: %d", strlen(s));
+    
+    return 0;
 }
-                    // If use &s
-                    /*The code might still compile, but it can cause
-                    warnings, confusing behavior, or even a crash,
-                    especially with newer compilers or on different
-                    systems.*/
+// If use &s
+/*The code might still compile, but it can cause
+warnings, confusing behavior, or even a crash,
+especially with newer compilers or on different
+systems.*/
