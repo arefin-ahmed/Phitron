887397dea1b1b0bc9f379747606ca22667b35bec
Arefin Ahmed <arefinahmed131@gmail.com>
2025-09-20 21:48:27 +0600

    use getline

---
+++ sem_1/week_3/Module 10 - String/B._Let's_use_Getline.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    char s[1000001];
+    fgets(s, sizeof(s), stdin);
+
+    for (int i = 0; s[i] != '\\'; i++)
+    {
+        printf("%c", s[i]);
+    }
+
+    return 0;
+}
