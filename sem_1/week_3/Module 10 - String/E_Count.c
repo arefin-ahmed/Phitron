0a2aa580817bcea6b5dcab945215efa722c08e6a
Arefin Ahmed <arefinahmed131@gmail.com>
2025-09-22 01:34:36 +0600

    summation of value in string

---
+++ sem_1/week_3/Module 10 - String/E_Count.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char s[1000001];
+    scanf("%s", s);
+    int sum = 0;
+    int length = strlen(s);
+
+    for (int i = 0; i < length; i++)
+    {
+        sum += s[i] - '0';    // to convert char to int we minus '0' or 48
+    }
+    printf("%d", sum);
+
+    return 0;
+}
