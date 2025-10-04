5fa8140a48502dbac5946ba15701881466ff3a88
Arefin Ahmed <arefinahmed131@gmail.com>
2025-10-04 23:25:53 +0600

    Reversing_the_string

--- sem_1/week_3/Module 10 - String/Reversing_the_string.c
+++ sem_1/week_3/Module 10 - String/Reversing_the_string.c
@@ -2,18 +2,18 @@
 int main()
 {
     int n;
-    int arr[101];
-
     scanf("%d", &n);
+    
+    int arr[n+1];
 
     for (int i = 0; i < n; i++)
     {
-        scanf("%s", &arr[i]);
+        scanf("%d", &arr[i]);
     }
 
     for (int i = n - 1; i >= 0; i--)
     {
-        printf("%s ", &arr[i]);
+        printf("%d ", arr[i]);
     }
 
     return 0;
