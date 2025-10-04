f7ce8db7170bef353ea362b76a3a78ba4b358033
Arefin Ahmed <arefinahmed131@gmail.com>
2025-10-04 19:37:14 +0600

    use string to find palindrome

--- sem_1/week_3/Module 10 - String/G_Palindrome_Array_1.c
+++ sem_1/week_3/Module 10 - String/G_Palindrome_Array_1.c
@@ -4,27 +4,30 @@
 int main()
 {
     int n;
-    scanf("%d", &n);
+    scanf("%d", &n);    
 
-    char s[n+1];
-    scanf("%s", s);   
+    char s[n + 1];
+    scanf("%s", s);     
 
-    int n = strlen(s);
-    int isPalindrome = 1; 
+    int isPalindrome = 1;
 
     for (int i = 0; i < n / 2; i++)
     {
         if (s[i] != s[n - 1 - i])
         {
-            isPalindrome = 0; 
+            isPalindrome = 0;
             break;
         }
     }
 
     if (isPalindrome)
-        printf("YES\n");
+        printf("YES");
     else
-        printf("NO\n");
+        printf("NO");
 
     return 0;
 }
+
+// Use char s[] + %s → for string palindrome.
+
+// Use int arr[] + %d → for integer array palindrome.
