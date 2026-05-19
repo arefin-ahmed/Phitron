d7ecd8ce42ecbc87a082061f86d026e8330bd027
arefin-ahmed <arefin-ahmed@users.noreply.github.com>
2026-05-19 10:03:17 +0600

    change s1 to a & s2 to b

--- sem_1/week_3/Module 11 - String Operation/String_concat_with_strcat_both_element.c
+++ sem_1/week_3/Module 11 - String Operation/String_concat_with_strcat_both_element.c
@@ -2,11 +2,11 @@
 #include <string.h>
 int main()
 {
-    char s1[101], s2[101];
-    scanf("%s %s", s1, s2);
+    char a[101], b[101];
+    scanf("%s %s", a, b);
 
-    strcat(s1, s2);         // Concatenate s2 into s1 ; String_concat_with_strcat_both_element
-    printf("%s %s", s1, s2);
+    strcat(a, b);         // Concatenate b into a ; String_concat_with_strcat_both_element
+    printf("%s %s", a, b);
 
     return 0;
 }
